5c864d1c3e55454b6fa6c7177f415e35e572a1a8
thiru <m.thiruvalluvar@gmail.com>
2022-11-09 07:53:03 +0800

    Status update with few more condition checking

--- recruitment_app/candidate/api/views.py
+++ recruitment_app/candidate/api/views.py
@@ -11,7 +11,12 @@ from .serializers import CandidateSerializer
 from django.db.models import Q
 from rest_framework.response import Response
 from rest_framework import status
+
 from ..models import STATUS_CHOICES
+import logging
+
+logger = logging.getLogger(__name__)
+
 class CandidateViewSet(generics.ListAPIView):
     model = CandidateProfile
     serializer_class = CandidateSerializer
@@ -25,14 +30,39 @@ class CandidateAPIUpdate(generics.UpdateAPIView):
     def update(self, request, *args, **kwargs):
         # The required object is obtained from django glossary
         # https://docs.djangoproject.com/en/3.1/glossary/
-        instance = self.get_object()
-        stage = str(request.data.get("stage"))
-        dict_status = dict(STATUS_CHOICES)
-        actual_status = dict_status.get(stage, None)
-        serializer = CandidateSerializer(instance, many=False)
-        if instance.stage != actual_status and actual_status:
-            instance.stage = actual_status
-            instance.save()
-        else:
-            return Response({'message':'fail due to wrong status','error':True,'code':400,'result':''})
-        return Response(serializer.data)
+        try:
+            instance = self.get_object()
+            logger.info(f'Candidate filtered to update {instance}')
+            stage = str(request.data.get("stage")) if request.data.get("stage", None) else None
+            dict_status = dict(STATUS_CHOICES)
+            actual_status = stage if stage in list(dict_status.keys()) else None
+            serializer = CandidateSerializer(instance, many=False)
+            if not stage:
+                return Response({'message': 'stage cannot be empty or null', 'error': True, 'code': 400, 'result': ''})
+            if not actual_status:
+                return Response({'message': 'stage not found!!', 'error': True, 'code': 400, 'result': ''})
+            if instance.stage == 'hired' or 'rejected' in instance.stage:
+                return Response({'message': 'Cannot Change the status of hired / rejected person ', 'error': True, 'code': 400, 'result': ''})
+            actual_status_index = list(dict_status.keys()).index(stage)
+            old_status_index = list(dict_status.keys()).index(instance.stage)
+            is_new_stage_set = instance.stage != actual_status
+            is_progressive_change = actual_status_index > old_status_index and not actual_status_index - old_status_index > 1
+            ignore_order_in_status_change = ['rejected_by_candidate', 'on_hold', 'rejected_by_company']
+            # cannot set same status for update, must be valid status , must  progress one step at a time and must not be in rejected status list
+            if instance.stage == 'advanced_interviewing' and actual_status == 'offered':
+                instance.stage = actual_status
+                instance.save()
+            elif instance.stage != actual_status and actual_status and is_progressive_change:
+                instance.stage = actual_status
+                instance.save()
+                logger.info(f'Candidate object {instance} updated with stage {stage} successfully !!')
+            # Can set rejected from any status and no need to check progressive for on_hold, cannot set same status and is not rejected
+            elif (stage in ignore_order_in_status_change or instance.stage == "on_hold") and is_new_stage_set:
+                instance.stage = actual_status
+                instance.save()
+                logger.info(f'Candidate object {instance} updated with stage {stage} successfully !!')
+            else:
+                return Response({'message': 'fail due to wrong status change', 'error': True, 'code': 400, 'result': ''})
+            return Response(serializer.data)
+        except Exception as ex:
+            return Response({'message': f'{ex}', 'error': True, 'code': 500, 'result': ''})
--- recruitment_app/candidate/tests.py
+++ recruitment_app/candidate/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
 
 # Create your tests here.
+import json
+from rest_framework import status
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import CandidateProfile
+from .api.serializers import CandidateSerializer
+from job_application.models import JobDetails
+
+# initialize the APIClient app
+client = Client()
+class GetAllTest(TestCase):
+    """ Test module for GET all puppies API """
+    #     first_name = models.CharField(null=False, blank=False, max_length=255)
+    #     last_name = models.CharField(null=False, blank=False, max_length=255)
+    #     email = models.EmailField(max_length=255)
+    #     job_id = models.ForeignKey(JobDetails, on_delete=models.CASCADE)
+    #     resume = models.BinaryField()
+    #     stage = models.CharField(max_length=150, choices=STATUS_CHOICES, default='pending')
+    # def setUp(self):
+    #
+    #     CandidateProfile.objects.all().delete()
+    #     print(JobDetails.objects.filter(id='045dbe33-0ea9-47b1-9e49-2a91caf15327'))
+    #     CandidateProfile.objects.create(
+    #         first_name='Casper', email='Casper@mailinator.com', last_name='Ronie', job_id=JobDetails.objects.filter(id='f69d784f-1b1c-40b5-a8ca-e976fe316821'))
+    #     CandidateProfile.objects.create(
+    #         first_name='Muffin', age=1, last_name='Chuk', job_id=JobDetails.objects.filter(id='f69d784f-1b1c-40b5-a8ca-e976fe316821'))
+    #     CandidateProfile.objects.create(
+    #         first_name='Rambo', age=2, last_name='Gazaki', job_id=JobDetails.objects.filter(id='f69d784f-1b1c-40b5-a8ca-e976fe316821'))
+    #     CandidateProfile.objects.create(
+    #         first_name='Ricky', age=6, last_name='Nick', job_id=JobDetails.objects.filter(id='f69d784f-1b1c-40b5-a8ca-e976fe316821'))
+
+    def test_get_all_candidate(self):
+        # get API response
+        response = client.get('/candidate/api/profile?format=json')
+        # get data from db
+        puppies = CandidateProfile.objects.all()
+        serializer = CandidateSerializer(puppies, many=True)
+        # print(self.assertEqual(response.data, serializer.data))
+        print(response.status_code, response.data)
+        print(self.assertEqual(response.status_code, status.HTTP_200_OK))
+
+
